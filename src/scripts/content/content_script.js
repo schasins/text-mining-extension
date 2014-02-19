38f4a5c881e6a9a73d11e82c03dd04dfebb23a90
schasins <schasins@gmail.com>
2014-02-19 14:39:41 -0800

    Some old changes for extracting keywords from the text a user has selected.  Very simple frequency-based keyword approach.

--- src/scripts/content/content_script.js
+++ src/scripts/content/content_script.js
@@ -1,19 +1,65 @@
 document.addEventListener('click', textMining, true);
 
-var bayes;
+$("body").select(function(){console.log("selected"); textMining();});
 
-bayes = new classifier.Bayesian();
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
 
-console.log(json.cats);
+var ignoreWords = ["","have","her","there","the","be","to","of","and","a","in","that","it","for","on","with","as","at","this","but","his","by","from","they","or","an","will","would","so","even","is","be","am","are"];
 
-bayes.backend.catCounts = json.cats;
-bayes.backend.wordCounts = json.words;
+function textMining(){
+  var text = getSelectionText().toLowerCase();
+  var wordCounts = { };
+  var words = text.split(/\s*[\s\.,-\/#!?$%\^&\*;:{}=\-_`~()–]\s*/);
 
-function textMining(eventData){
-  var target = eventData.target;
-  var innerText = target.innerText;
+  for(var i = 0; i < words.length; i++){
+    word = words[i];
+    if ($.inArray(word, ignoreWords) > -1){
+		continue;
+	}
+	if (wordCounts["_" + word]){
+		wordCounts["_" + word] = wordCounts["_" + word] + 1;
+	}
+	else {
+		wordCounts["_" + word] = 1;
+	}
+  }
+  
+  //deal with easy plurals
+  for (var word in wordCounts){
+	  if(wordCounts[word+"s"]){
+		  wordCounts[word] = wordCounts[word]+wordCounts[word+"s"];
+		  delete wordCounts[word+"s"];
+	  }
+  }
+  
+  console.log(wordCounts);
+  var tuples = [];
 
-  var cat = bayes.classify(innerText);
-  alert(cat + ": " + innerText);
+  for (var key in wordCounts) tuples.push([key, wordCounts[key]]);
 
+  tuples.sort(function(a, b) {
+    a = a[1];
+    b = b[1];
+
+    return a > b ? -1 : (a < b ? 1 : 0);
+  });
+  
+  var count = 0;
+  var str = "";
+  for (var i = 0; i<tuples.length; i++){
+	  count ++;
+	  if (count > 20 || tuples[i][1] < 2){
+		  break;
+	  }
+	  str += "\""+tuples[i][0].slice(1,tuples[i][0].length)+"\", ";
+  }
+  alert(str);
 }
